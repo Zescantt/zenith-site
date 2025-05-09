df21be379c4043fc655f6f56008ca434d52fbc69
Zescantt <only.nugget0@gmail.com>
2025-05-09 17:53:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("section").style.opacity = "1";
+  document.querySelector("section").style.transform = "translateY(0)";
+});
